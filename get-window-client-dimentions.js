452f4f12373de4d2837b1024eaa895edd4c7409f
Timber <timhjellum@gmail.com>
2023-08-11 19:19:39 -0700

    Update get-window-client-dimentions.js

---
+++ get-window-client-dimentions.js
@@ -0,0 +1,26 @@
+const isSupported = () => typeof window !== 'undefined';
+const getWindowWidth = () => {
+  if (!isSupported()) {
+    return 0;
+  }
+  return (
+    document.getElementsByTagName('body')[0].clientWidth
+  )
+}
+const getActualWidth = () => {
+  if (!isSupported()) {
+    return 0;
+  }
+  return (
+    window.innerWidth
+  )
+}
+
+window.addEventListener('scroll', runScript)
+window.addEventListener('resize', runScript)
+window.addEventListener('load'', runScript)
+
+function runScript() {
+  console.log(getWindowWidth() + 'px')
+  console.log(getActualWidth() + 'px')
+}
